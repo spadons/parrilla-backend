feab162ecfe6a71fcdca8d328115d690c04d60e2
Osvaldo Rivas <spadons@gmail.com>
2021-08-15 00:50:44 -0400

    test

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const Max6675 = require('max6675-raspi');
+const Max6675 = require('max6675-raspi');
 
 const app = express()
 const port = 3000
@@ -9,7 +9,7 @@ const SCK = 24;
 const SO = [25];
 const UNIT = 1;
 //const max6675 = new Max6675();
-max6675.setPin(CS,SCK,SO,UNIT);
+//max6675.setPin(CS,SCK,SO,UNIT);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
