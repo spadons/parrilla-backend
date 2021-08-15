1fcf232bc8032f53c6988dbb0a85a5c4b50ed8e4
Osvaldo Rivas <spadons@gmail.com>
2021-08-15 00:15:06 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const Max6675 = require("max6675-raspi");
+
+const app = express()
+const port = 3000
+
+const CS = 4;
+const SCK = 24;
+const SO = [25];
+const UNIT = 1;
+const max6675 = new Max6675();
+max6675.setPin(CS,SCK,SO,UNIT);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+
+  (async () => {
+    while (true){
+        const {temp, unit} = max6675.readTemp();
+        if (temp.length)
+        tempText.textContent = temp + "ºC";
+        console.log(tempText);
+        await max6675.sleep(2000);
+    }
+})();
+
+})
+
+
+
+
