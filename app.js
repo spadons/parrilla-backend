b2cf3e08ca71d25d6f18fe4e1cc7c6df4ec160fb
Osvaldo Rivas <spadons@gmail.com>
2021-08-15 00:24:12 -0400

    Test

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Max6675 = require("max6675-raspi");
+const Max6675 = require('max6675-raspi');
 
 const app = express()
 const port = 3000
@@ -16,17 +16,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-
-  (async () => {
-    while (true){
-        const {temp, unit} = max6675.readTemp();
-        if (temp.length)
-        tempText.textContent = temp + "ºC";
-        console.log(tempText);
-        await max6675.sleep(2000);
-    }
-})();
+  console.log(`Example app listening at http://localhost:${port}`);
 
 })
 
